160e7b7ec8fff26694dc46b0bdcc042aa31df80f
Jonah Zukosky <jonah.zukosky@gmail.com>
2020-05-19 15:46:44 -0500

    Delete uploadCommand.sh

--- reactionHandler/uploadCommand.sh
+++
@@ -1,3 +0,0 @@
-zip -r lambda.zip . && aws lambda update-function-code --function-name arn:aws:lambda:us-east-1:087468321421:function:auggie-reaction-handler --zip-file fileb://lambda.zip --profile augur --region us-east-1
-
-
