2af53f5b17e33b5b3611c253f276e1ccc5ddd8b6
Jonah Zukosky <jonah.zukosky@gmail.com>
2020-05-19 15:35:32 -0500

    filtering reactions to only auggie's messages

--- reactionHandler/index.js
+++ reactionHandler/index.js
@@ -9,16 +9,61 @@ AWS.config.update({
 });
 let docClient = new AWS.DynamoDB.DocumentClient();
 
+async function getToken(teamID) {
+
+    let users = [];
+    let params = { TableName: process.env.TABLE_NAME };
+
+    while (true) {
+        let response = await docClient.scan(params).promise();
+        users = users.concat(response.Items);
+        if (response.LastEvaluatedKey) {
+            params.ExclusiveStartKey = response.LastEvaluatedKey
+            continue;
+        } else {
+            break;
+        }
+    }
+
+    for (user of users) {
+        if (user.teamID == teamID) {
+            return user.botToken;
+        }
+    }
+
+    return null;  
+}
+
 
 exports.handler = async (event) => {
 
     console.log(event);
 
-    let slackEvent = event.event;
+    const slackEvent = event.event;
+
+    const token = await getToken(event["team_id"]);
+    const slackClient = new WebClient(token);
+
+    const slackResponse = await slackClient.conversations.replies({
+        channel: slackEvent.item.channel,
+        ts: slackEvent.item.ts
+    });
+
+    const reactedMessage = slackResponse.messages[0];
+
+    if (!reactedMessage["bot_id"] || reactedMessage["bot_id"] != "BSNDG3W1E") {
+        console.log("Reacted Message was not from Auggie.");
+        return;
+    }
+
     console.log(`Received Reaction Notification: ${slackEvent.reaction} on message \n${JSON.stringify(slackEvent.item)}`);
+
+
+
+
     const response = {
         statusCode: 200,
-        body: `Received Reaction Notification: ${slackEvent.reaction} on message \n${JSON.stringify(slackEvent.item)}`,
+        body: `Received Reaction Notification: ${slackEvent.reaction} on message \n${slackEvent.item}`,
     };
     return response;
 };
